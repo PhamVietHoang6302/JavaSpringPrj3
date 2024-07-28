01aaaf398d3a04b74725eeb02eab7cc3793f74fe
Vy Do <donhuvy@outlook.com>
2024-07-28 19:06:24 +0700

    fix unicode

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Microsoft Windows [Version 10.0.22631.3880]
 
 C:\GitHub\JavaSpringPrj3>docker compose -f docker-compose.yml up
 ```
+
+
+http://localhost:8081/admin/building-list
+
+```bash
+nguyenvana
+
+123456
+```
+
--- database/insert_database.sql
+++ database/insert_database.sql
@@ -1,21 +1,26 @@
-use springbootweb;
+use estateadvance;
 
-insert into role(code,name) values('MANAGER','Quản trị hệ thống');
-insert into role(code,name) values('USER','người dùng');
+insert into role(code, name)
+values ('MANAGER', 'Quan tri he thong');
+insert into role(code, name)
+values ('USER', 'Nguoi dung');
 
-insert into user(username,password,fullname,status)
-values('admin','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','admin',1);
-insert into user(username,password,fullname,status)
-values('nguyenvana','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyen van a',1);
-insert into user(username,password,fullname,status)
-values('nguyenvanb','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyen van b',1);
+insert into user(username, password, fullname, status)
+values ('admin', '$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG', 'admin', 1);
+insert into user(username, password, fullname, status)
+values ('nguyenvana', '$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG', 'nguyen van a', 1);
+insert into user(username, password, fullname, status)
+values ('nguyenvanb', '$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG', 'nguyen van b', 1);
 
-INSERT INTO user_role(user_id,role_id) VALUES (1,1);
-INSERT INTO user_role(user_id,role_id) VALUES (2,2);
-INSERT INTO user_role(user_id,role_id) VALUES (3,2);
+INSERT INTO user_role(user_id, role_id)
+VALUES (1, 1);
+INSERT INTO user_role(user_id, role_id)
+VALUES (2, 2);
+INSERT INTO user_role(user_id, role_id)
+VALUES (3, 2);
 
 
-
-
-
+select * from user;
+select * from role;
+select * from user_role;
 
